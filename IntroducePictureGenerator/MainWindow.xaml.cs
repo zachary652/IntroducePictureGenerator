25b85ebe90c68d74f26d2e43c3553bcfc9d12271
zacharyzhu <57157314@163.com>
2018-04-16 23:17:33 +0800

    添加项目文件。

---
+++ IntroducePictureGenerator/MainWindow.xaml.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ColorFont;
+using Microsoft.Win32;
+using Xceed.Wpf.Toolkit;
+
+namespace IntroducePictureGenerator
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<TextBox> SpotList;
+
+        public List<TextBox> SpotDetailList;
+
+        public List<Xceed.Wpf.Toolkit.ColorPicker> SpotIndexColorList;
+
+        public FontInfo TitleFontInfo;
+
+        public FontInfo TitleDetailFontInfo;
+
+        public FontInfo SpotFontInfo;
+
+        public FontInfo SpotDetailFontInfo;
+
+        PreviewWindow _previewWindow;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SpotList = new List<TextBox>();
+            SpotDetailList = new List<TextBox>();
+            SpotIndexColorList = new List<Xceed.Wpf.Toolkit.ColorPicker>();
+            TitleFontInfo = FontInfo.GetControlFont(this.TextBox_Title);
+            TitleDetailFontInfo = FontInfo.GetControlFont(this.TextBox_Title);
+            SpotFontInfo = FontInfo.GetControlFont(this.TextBox_Title);
+            SpotDetailFontInfo = FontInfo.GetControlFont(this.TextBox_Title);
+            Button_Generate.IsEnabled = false;
+        }
+
+        private void AddSpot()
+        {
+            Grid grid = new Grid();
+            grid.Margin = new Thickness(15);
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(3, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+            TextBlock textBlock = new TextBlock() { Text = $"特点{SpotList.Count + 1}" };
+            textBlock.VerticalAlignment = VerticalAlignment.Center;
+            textBlock.HorizontalAlignment = HorizontalAlignment.Center;
+            textBlock.SetValue(Grid.RowProperty, 0);
+            textBlock.SetValue(Grid.ColumnProperty, 0);
+            TextBox textBox = new TextBox();
+            textBox.VerticalAlignment = VerticalAlignment.Center;
+            textBox.SetValue(Grid.RowProperty, 0);
+            textBox.SetValue(Grid.ColumnProperty, 1);
+            textBox.Margin = new Thickness(0, 0, 95, 0);
+            TextBlock textBlockColor = new TextBlock();
+            textBlockColor.Text = "序号颜色";
+            textBlockColor.VerticalAlignment = VerticalAlignment.Center;
+            textBlockColor.HorizontalAlignment = HorizontalAlignment.Right;
+            textBlockColor.SetValue(Grid.RowProperty, 0);
+            textBlockColor.SetValue(Grid.ColumnProperty, 1);
+            textBlockColor.Margin = new Thickness(0, 0, 40, 0);
+            Xceed.Wpf.Toolkit.ColorPicker colorPicker = new Xceed.Wpf.Toolkit.ColorPicker();
+            colorPicker.Width = 35;
+            colorPicker.VerticalAlignment = VerticalAlignment.Center;
+            colorPicker.HorizontalAlignment = HorizontalAlignment.Right;
+            colorPicker.SetValue(Grid.RowProperty, 0);
+            colorPicker.SetValue(Grid.ColumnProperty, 1);
+            grid.Children.Add(textBlock);
+            grid.Children.Add(textBox);
+            grid.Children.Add(textBlockColor);
+            grid.Children.Add(colorPicker);
+            SpotList.Add(textBox);
+            SpotIndexColorList.Add(colorPicker);
+
+            TextBlock textBlockDetail = new TextBlock() { Text = $"特点详细{SpotList.Count}" };
+            textBlockDetail.VerticalAlignment = VerticalAlignment.Center;
+            textBlockDetail.HorizontalAlignment = HorizontalAlignment.Center;
+            textBlockDetail.SetValue(Grid.RowProperty, 1);
+            textBlockDetail.SetValue(Grid.ColumnProperty, 0);
+            textBlockDetail.Margin = new Thickness(0, 5, 0, 0);
+            TextBox textBoxDetail = new TextBox();
+            textBoxDetail.VerticalAlignment = VerticalAlignment.Center;
+            textBoxDetail.SetValue(Grid.RowProperty, 1);
+            textBoxDetail.SetValue(Grid.ColumnProperty, 1);
+            textBoxDetail.Margin = new Thickness(0, 5, 0, 0);
+            textBoxDetail.AcceptsReturn = true;
+            grid.Children.Add(textBlockDetail);
+            grid.Children.Add(textBoxDetail);
+            SpotDetailList.Add(textBoxDetail);
+
+            int index = containerStackPanel.Children.IndexOf(addDetailGrid);
+            containerStackPanel.Children.Insert(index, grid);
+
+            Button_SpotFont.IsEnabled = true;
+            Button_SpotDetailFont.IsEnabled = true;
+        }
+
+        private void RemoveSpot()
+        {
+            int index = containerStackPanel.Children.IndexOf(addDetailGrid);
+            if (index > 1)
+            {
+                containerStackPanel.Children.RemoveAt(index - 1);
+                SpotList.RemoveAt(SpotList.Count - 1);
+                SpotDetailList.RemoveAt(SpotDetailList.Count - 1);
+                SpotIndexColorList.RemoveAt(SpotIndexColorList.Count - 1);
+            }
+            if (containerStackPanel.Children.IndexOf(addDetailGrid) == 1)
+            {
+                Button_SpotFont.IsEnabled = false;
+                Button_SpotDetailFont.IsEnabled = false;
+            }
+        }
+
+        private void addDetailButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddSpot();
+        }
+
+        private void removeDetailButton_Click(object sender, RoutedEventArgs e)
+        {
+            RemoveSpot();
+        }
+
+        private void Button_TitleFont_Click(object sender, RoutedEventArgs e)
+        {
+            ColorFont.ColorFontDialog fntDialog = new ColorFont.ColorFontDialog();
+            fntDialog.Owner = this;
+            fntDialog.Font = FontInfo.GetControlFont(this.TextBox_Title);
+            if (fntDialog.ShowDialog() == true)
+            {
+                TitleFontInfo = fntDialog.Font;
+            }
+        }
+
+        private void Button_TitleDetailFont_Click(object sender, RoutedEventArgs e)
+        {
+            ColorFont.ColorFontDialog fntDialog = new ColorFont.ColorFontDialog();
+            fntDialog.Owner = this;
+            fntDialog.Font = FontInfo.GetControlFont(this.TextBox_TitleDetail);
+            if (fntDialog.ShowDialog() == true)
+            {
+                TitleDetailFontInfo = fntDialog.Font;
+            }
+        }
+
+        private void Button_SpotFont_Click(object sender, RoutedEventArgs e)
+        {
+            ColorFont.ColorFontDialog fntDialog = new ColorFont.ColorFontDialog();
+            fntDialog.Owner = this;
+            fntDialog.Font = FontInfo.GetControlFont(this.SpotList[0]);
+            if (fntDialog.ShowDialog() == true)
+            {
+                SpotFontInfo = fntDialog.Font;
+            }
+        }
+
+        private void Button_SpotDetailFont_Click(object sender, RoutedEventArgs e)
+        {
+            ColorFont.ColorFontDialog fntDialog = new ColorFont.ColorFontDialog();
+            fntDialog.Owner = this;
+            fntDialog.Font = FontInfo.GetControlFont(this.SpotDetailList[0]);
+            if (fntDialog.ShowDialog() == true)
+            {
+                SpotDetailFontInfo = fntDialog.Font;
+            }
+        }
+
+        private void Button_Generate_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PNG文件(*.png)|*.png";
+            if (sfd.ShowDialog() == true)
+            {
+                RenderTargetBitmap renderBitmap = new RenderTargetBitmap((int)_previewWindow.dragCanvas.ActualWidth, (int)_previewWindow.dragCanvas.ActualHeight, 96d, 96d, PixelFormats.Pbgra32);
+                renderBitmap.Render(_previewWindow.dragCanvas);
+                using (FileStream outStream = new FileStream(sfd.FileName, FileMode.Create))
+                {
+                    PngBitmapEncoder encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+                    encoder.Save(outStream);
+                }
+            }
+        }
+
+        private void Button_Preview_Click(object sender, RoutedEventArgs e)
+        {
+            if (_previewWindow == null)
+            {
+                _previewWindow = new PreviewWindow(this);
+                _previewWindow.Closed += _previewWindow_Closed;
+                _previewWindow.Show();
+                _previewWindow.GeneratePreview();
+            }
+            else
+            {
+                _previewWindow.GeneratePreview();
+            }
+            Button_Generate.IsEnabled = true;
+        }
+
+        private void _previewWindow_Closed(object sender, EventArgs e)
+        {
+            _previewWindow = null;
+            Button_Generate.IsEnabled = false;
+        }
+
+        private void Button_BrowseImg1_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_ImgPath1.Text = SelectPicture();
+        }
+
+        private void Button_BrowseImg2_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_ImgPath2.Text = SelectPicture();
+        }
+
+        private void Button_BrowseImg3_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_ImgPath3.Text = SelectPicture();
+        }
+
+        private void Button_BrowseImg4_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_ImgPath4.Text = SelectPicture();
+        }
+
+        private void Button_BrowseImg5_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_ImgPath5.Text = SelectPicture();
+        }
+
+        private string SelectPicture()
+        {
+            OpenFileDialog sfd = new OpenFileDialog();
+            sfd.Filter = "PNG文件(*.png)|*.png|JPEG文件(*.jpg)|*.jpg|BMP文件(*.bmp)|*.bmp";
+            if (sfd.ShowDialog() == true)
+            {
+                return sfd.FileName;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ IntroducePictureGenerator/PreviewWindow.xaml.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IntroducePictureGenerator
+{
+    /// <summary>
+    /// PreviewWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class PreviewWindow : Window
+    {
+        private const double GOLDEN_SECTION = 0.618d;
+
+        private MainWindow _mainWindow;
+
+        private double _width;
+
+        private double _height;
+
+        public PreviewWindow(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+
+        public void GeneratePreview()
+        {
+            _width = dragCanvas.ActualWidth;
+            _height = dragCanvas.ActualHeight;
+            dragCanvas.Children.Clear();
+            double imageAreaWitdth = _width * GOLDEN_SECTION;
+            if (File.Exists(_mainWindow.TextBox_ImgPath1.Text))
+            {
+                Image image = new Image();
+                image.SetValue(Canvas.LeftProperty, 5d);
+                image.SetValue(Canvas.TopProperty, 5d);
+                image.Width = imageAreaWitdth / 2 - 10;
+                image.Height = _height / 3 - 10;
+                image.Stretch = Stretch.Fill;
+                image.UseLayoutRounding = true;
+                image.SetValue(RenderOptions.BitmapScalingModeProperty, BitmapScalingMode.HighQuality);
+                image.Source = new BitmapImage(new Uri(_mainWindow.TextBox_ImgPath1.Text, UriKind.Absolute));
+                dragCanvas.Children.Add(image);
+            }
+            if (File.Exists(_mainWindow.TextBox_ImgPath2.Text))
+            {
+                Image image = new Image();
+                image.SetValue(Canvas.LeftProperty, imageAreaWitdth / 2 + 5);
+                image.SetValue(Canvas.TopProperty, 5d);
+                image.Width = imageAreaWitdth / 2 - 10;
+                image.Height = _height / 3 - 10;
+                image.Stretch = Stretch.Fill;
+                image.UseLayoutRounding = true;
+                image.SetValue(RenderOptions.BitmapScalingModeProperty, BitmapScalingMode.HighQuality);
+                image.Source = new BitmapImage(new Uri(_mainWindow.TextBox_ImgPath2.Text, UriKind.Absolute));
+                dragCanvas.Children.Add(image);
+            }
+            if (File.Exists(_mainWindow.TextBox_ImgPath3.Text))
+            {
+                Image image = new Image();
+                image.SetValue(Canvas.LeftProperty, 5d);
+                image.SetValue(Canvas.TopProperty, _height / 3 + 5);
+                image.Width = imageAreaWitdth / 2 - 10;
+                image.Height = _height / 3 - 10;
+                image.Stretch = Stretch.Fill;
+                image.UseLayoutRounding = true;
+                image.SetValue(RenderOptions.BitmapScalingModeProperty, BitmapScalingMode.HighQuality);
+                image.Source = new BitmapImage(new Uri(_mainWindow.TextBox_ImgPath3.Text, UriKind.Absolute));
+                dragCanvas.Children.Add(image);
+            }
+            if (File.Exists(_mainWindow.TextBox_ImgPath4.Text))
+            {
+                Image image = new Image();
+                image.SetValue(Canvas.LeftProperty, imageAreaWitdth / 2 + 5);
+                image.SetValue(Canvas.TopProperty, _height / 3 + 5);
+                image.Width = imageAreaWitdth / 2 - 10;
+                image.Height = _height / 3 - 10;
+                image.Stretch = Stretch.Fill;
+                image.UseLayoutRounding = true;
+                image.SetValue(RenderOptions.BitmapScalingModeProperty, BitmapScalingMode.HighQuality);
+                image.Source = new BitmapImage(new Uri(_mainWindow.TextBox_ImgPath4.Text, UriKind.Absolute));
+                dragCanvas.Children.Add(image);
+            }
+            if (File.Exists(_mainWindow.TextBox_ImgPath5.Text))
+            {
+                Image image = new Image();
+                image.SetValue(Canvas.LeftProperty, 5d);
+                image.SetValue(Canvas.TopProperty, _height / 3 * 2 + 5);
+                image.Width = imageAreaWitdth - 10;
+                image.Height = _height / 3 - 15;
+                image.Stretch = Stretch.Fill;
+                image.UseLayoutRounding = true;
+                image.SetValue(RenderOptions.BitmapScalingModeProperty, BitmapScalingMode.HighQuality);
+                image.Source = new BitmapImage(new Uri(_mainWindow.TextBox_ImgPath5.Text, UriKind.Absolute));
+                dragCanvas.Children.Add(image);
+            }
+            double textAreaWidthOffset = _width * GOLDEN_SECTION;
+            double textAreaHeightOffset = 100d;
+            TextBlock textBlockTitle = new TextBlock();
+            textBlockTitle.Text = _mainWindow.TextBox_Title.Text;
+            textBlockTitle.FontFamily = _mainWindow.TitleFontInfo.Family;
+            textBlockTitle.FontSize = _mainWindow.TitleFontInfo.Size;
+            textBlockTitle.FontWeight = _mainWindow.TitleFontInfo.Weight;
+            textBlockTitle.SetValue(Canvas.LeftProperty, 50d + textAreaWidthOffset);
+            textBlockTitle.SetValue(Canvas.TopProperty, textAreaHeightOffset);
+            dragCanvas.Children.Add(textBlockTitle);
+            this.UpdateLayout();
+            textAreaHeightOffset += textBlockTitle.ActualHeight + 10;
+
+            TextBlock textBlockTitleDetail = new TextBlock();
+            textBlockTitleDetail.Text = _mainWindow.TextBox_TitleDetail.Text;
+            textBlockTitleDetail.FontFamily = _mainWindow.TitleDetailFontInfo.Family;
+            textBlockTitleDetail.FontSize = _mainWindow.TitleDetailFontInfo.Size;
+            textBlockTitleDetail.FontWeight = _mainWindow.TitleDetailFontInfo.Weight;
+            textBlockTitleDetail.SetValue(Canvas.LeftProperty, 50d + textAreaWidthOffset);
+            textBlockTitleDetail.SetValue(Canvas.TopProperty, textAreaHeightOffset);
+            dragCanvas.Children.Add(textBlockTitleDetail);
+            this.UpdateLayout();
+            textAreaHeightOffset += textBlockTitleDetail.ActualHeight + 25;
+
+            for (int i = 0; i < _mainWindow.SpotList.Count; i++)
+            {
+                TextBlock textIndex = new TextBlock();
+                textIndex.Text = (i + 1).ToString();
+                textIndex.FontFamily = _mainWindow.SpotFontInfo.Family;
+                textIndex.FontSize = _mainWindow.SpotFontInfo.Size;
+                textIndex.FontWeight = _mainWindow.SpotFontInfo.Weight;
+                textIndex.Foreground = Brushes.White;
+                textIndex.SetValue(Canvas.LeftProperty, 40d + textAreaWidthOffset);
+                textIndex.SetValue(Canvas.TopProperty, textAreaHeightOffset);
+                textIndex.SetValue(Canvas.ZIndexProperty, 1);
+                dragCanvas.Children.Add(textIndex);
+                this.UpdateLayout();
+
+                Ellipse ellipse = new Ellipse();
+                if (_mainWindow.SpotIndexColorList[i].SelectedColor != null)
+                {
+                    ellipse.Fill = new SolidColorBrush((Color)_mainWindow.SpotIndexColorList[i].SelectedColor);
+                }
+                ellipse.Width = textIndex.ActualHeight * 2;
+                ellipse.Height = textIndex.ActualHeight * 2;
+                ellipse.SetValue(Canvas.LeftProperty, 40d + textAreaWidthOffset - (ellipse.Width - textIndex.ActualWidth) / 2);
+                ellipse.SetValue(Canvas.TopProperty, textAreaHeightOffset - (ellipse.Height - textIndex.ActualHeight) / 2);
+                dragCanvas.Children.Insert(dragCanvas.Children.IndexOf(textIndex), ellipse);
+                this.UpdateLayout();
+
+                TextBlock textSpot = new TextBlock();
+                textSpot.Text = _mainWindow.SpotList[i].Text;
+                textSpot.FontFamily = _mainWindow.SpotFontInfo.Family;
+                textSpot.FontSize = _mainWindow.SpotFontInfo.Size;
+                textSpot.FontWeight = _mainWindow.SpotFontInfo.Weight;
+                textSpot.SetValue(Canvas.LeftProperty, 40d + textAreaWidthOffset + textIndex.ActualWidth + 30);
+                textSpot.SetValue(Canvas.TopProperty, textAreaHeightOffset);
+                dragCanvas.Children.Add(textSpot);
+                this.UpdateLayout();
+
+                textAreaHeightOffset += textSpot.ActualHeight + 20;
+
+                TextBlock textSpotDetail = new TextBlock();
+                textSpotDetail.Text = _mainWindow.SpotDetailList[i].Text;
+                textSpotDetail.FontFamily = _mainWindow.SpotDetailFontInfo.Family;
+                textSpotDetail.FontSize = _mainWindow.SpotDetailFontInfo.Size;
+                textSpotDetail.FontWeight = _mainWindow.SpotDetailFontInfo.Weight;
+                textSpotDetail.SetValue(Canvas.LeftProperty, 40d + textAreaWidthOffset + textIndex.ActualWidth + 30);
+                textSpotDetail.SetValue(Canvas.TopProperty, textAreaHeightOffset);
+                dragCanvas.Children.Add(textSpotDetail);
+                this.UpdateLayout();
+
+                textAreaHeightOffset += textSpotDetail.ActualHeight + 20;
+            }
+
+
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            GeneratePreview();
+        }
+    }
+}
